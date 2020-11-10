5e5c417ec21c1f10a215e1df2c58284f97bacc09
tech-chieftain <binaryminded@gmail.com>
2020-11-10 15:16:32 +0300

    Fixed errors in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import './styles/main.css';
-import Home from './containers/Home';
-import About from './containers/About';
-import Instructions from './containers/Instruction';
-import Process from './containers/Process';
-import Search from './containers/Search';
-import Donate from './containers/Donate';
-import DontDonate from './containers/DontDonate';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer/Footer';
 
 function App() {
   // This controls the direction of the page for RTL languages
@@ -19,11 +9,7 @@ function App() {
     document.dir = i18n.dir();
   }, [i18n, i18n.language]);
 
-  return (
-    <div>
-      App.js
-    </div>
-    );
+  return <div>App.js</div>;
 }
 
 export default App;
